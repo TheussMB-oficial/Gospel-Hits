86b6a747c9d8c69f3b034bcf286f311b82189435
Matheus B Moreira <96734989+TheussMB-oficial@users.noreply.github.com>
2023-11-16 10:18:10 -0300

    Create Gospel Hits.py

---
+++ Gospel Hits.py
@@ -0,0 +1,12 @@
+# Importar o módulo webbrowser para abrir o link no navegador
+import webbrowser
+
+# Definir o link como uma variável
+link = "https://radiogospelhits.com.br"
+
+# Criar uma função que abre o link no navegador padrão
+def abrir_link():
+    webbrowser.open(link)
+
+# Chamar a função
+abrir_link()
